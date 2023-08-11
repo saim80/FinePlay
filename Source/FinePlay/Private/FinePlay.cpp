736e6b5d616c3d7fa884069e2744b92a61d38885
Sangwoo Im <saim80@me.com>
2023-08-11 13:26:34 -0400

    Project initialized

---
+++ Source/FinePlay/Private/FinePlay.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FinePlay.h"
+
+#define LOCTEXT_NAMESPACE "FFinePlayModule"
+
+void FFinePlayModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FFinePlayModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FFinePlayModule, FinePlay)
---
+++ Source/FinePlay/Public/FinePlayLog.h
@@ -0,0 +1,16 @@
+﻿// (c) 2023 Pururum LLC. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FineBaseLoggerMacros.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogFinePlay, Log, All);
+
+#define FP_FATAL(FORMAT, ...) FINEBASE_FATAL(LogFinePlay, FORMAT, ##__VA_ARGS__)
+#define FP_ERROR(FORMAT, ...) FINEBASE_ERROR(LogFinePlay, FORMAT, ##__VA_ARGS__)
+#define FP_WARNING(FORMAT, ...) FINEBASE_WARNING(LogFinePlay, FORMAT, ##__VA_ARGS__)
+#define FP_DISPLAY(FORMAT, ...) FINEBASE_DISPLAY(LogFinePlay, FORMAT, ##__VA_ARGS__)
+#define FP_LOG(VERBOSITY, FORMAT, ...) FINEBASE_LOG(LogFinePlay, VERBOSITY, FORMAT, ##__VA_ARGS__)
+#define FP_VERBOSE(FORMAT, ...) FINEBASE_VERBOSE(LogFinePlay, FORMAT, ##__VA_ARGS__)
+#define FP_VERYVERBOSE(FORMAT, ...) FINEBASE_VERYVERBOSE(LogFinePlay, FORMAT, ##__VA_ARGS__)
